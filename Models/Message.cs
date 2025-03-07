64bcb7d3ab6dd0bc9d111a3189280b84b34dfd2a
AliceLJY <136287420+AliceLJY@users.noreply.github.com>
2025-03-07 22:23:02 +0800

    添加Message.cs模型

---
+++ Models/Message.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GrokBot.Api.Models
+{
+    public class Message
+    {
+        public string Role { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+    }
+}
